192fec60528379847f2a65d1480f78aed67f3d39
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-07 16:23:25 +0530

    Create mathi4(7).c

---
+++ mathi4(7).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.c>
+int main main()
+{
+char a;
+clrscr;
+printf("Enter a character ",a);
+scanf("%c",&a);
+if((a>='a'&&a<='z')||(a>='A'&&a<='z'))
+{
+printf("The character which you entered is an Alphabet",a);
+}
+else
+{
+printf("This is not an alphabet",a);
+return 0;
+getch;
+}
