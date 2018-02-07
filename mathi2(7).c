bdc96fc4f12d833f60f2d08fdfcaa56f58666f5f
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-07 16:14:41 +0530

    Create mathi2(7).c

---
+++ mathi2(7).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.c>
+int main ()
+{
+    char b;
+    clrscr ;
+    if(b=='a'|b=='e'|b=='i'|b=='o'|b=='u')
+    {
+    printf("Vowel");
+    }
+    else 
+    {
+        printf("Consonent");
+        return 0;
+        getch;
+        }
+    
