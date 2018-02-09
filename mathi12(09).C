788b5071cde77b4b5b36de77df175b518b7d4d9a
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-09 13:48:51 +0530

    Create mathi12(09).C

---
+++ mathi12(09).C
@@ -0,0 +1,19 @@
+#include<stio.c>
+#include<conio.h>
+int main()
+{
+num=int(raw_input("Enter the number"))
+n1=num
+n=0
+while(num>0):
+        d=num%10
+        n=n*10+d
+        num=num/10
+if(n==n1):
+        print("It is a palindrome")
+else:
+        print("It is  not a palindrome")
+        getchch;
+        return 0;
+        }
+        
