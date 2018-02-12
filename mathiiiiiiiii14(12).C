6b9f577be14922073b851d37a77ff810eed0ef3e
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-12 16:09:50 +0530

    Create mathiiiiiiiii14(12).C

---
+++ mathiiiiiiiii14(12).C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#inclide<conio.h>
+0 int main (
+ {
+ clrscr 
+ a=input()
+ b=input()
+ odds=[]
+ even=[]
+ if(a.isnumeric()):
+	a=int(a)
+	if(b.isnumeric()):
+		b=int(b)
+ 		for i in range(a,b):
+ 			if(i%2!=0):
+ 				odds.append(i)
+ 			else:
+ 				even.append(i)
+ 		printf(dds);		
+ 	else:
+ 		print("invalid");
+ else:
+ 
+printf"invalid");
+getch;
+return 0;
+{
