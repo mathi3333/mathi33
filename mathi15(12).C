7744cbc66a7dc97428130a675471db058805b36e
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-12 16:14:59 +0530

    Create mathi15(12).C

---
+++ mathi15(12).C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a,b,i;
+printf("enter the two limits");
+scanf("%d%d",&a,&b);
+for(i=a;i<b;i++)
+{
+if((i)%2==0)
+printf("%d\n",i);
+getch;
+return 0;
+}
