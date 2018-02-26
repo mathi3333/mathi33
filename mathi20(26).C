649344b73cdb43e299ff078e7576861b068d9b14
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-26 16:23:20 +0530

    Create mathi20(26).C

---
+++ mathi20(26).C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+clrscr ;
+int n,sum=0,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+sum=i*n;
+
+printf("%d",sum);
+return 0;
+getch;
+}}
