cff96604632fa5da6a03eaf30635d98c9a7a278d
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-28 16:27:24 +0530

    Create mathi24(28).C

---
+++ mathi24(28).C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+CLRSCR;
+int a[10],i,j,n,temp;
+printf("enter limit");
+scanf("%d",&n);
+printf(" enter the number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("sorted array");
+for(i=0;i<n;i++)
+{
+for(j=1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}}
+}
+for(i=0;i<n;i++)
+{
+printf("%d\n",a[i]);
+RETURN 0;
+GETCH;
+}
+}
