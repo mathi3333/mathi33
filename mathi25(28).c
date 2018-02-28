6ce4ec967756ae691c2fa038091e6e5374a33b4c
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-28 16:29:43 +0530

    Create mathi25(28).c

---
+++ mathi25(28).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+clrscr;
+    int a[10],i,n,k,sum=0;
+    printf("enter the limit");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    k=sum/n;
+    printf("%d",k);
+  return 0;
+  getch;
+    
+}
