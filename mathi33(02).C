f352730bfc368f4815e4c2449dbfeb9ccbc1c715
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-02 14:22:52 +0530

    Create mathi33(02).C

---
+++ mathi33(02).C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) 
+{
+clrscr;
+	char str[50];
+	int i,count=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nno of white spaces=%d",count);
+	return 0;
+  getch;
+}
