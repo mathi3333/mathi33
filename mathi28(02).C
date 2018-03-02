26ce74820035daf3c8ce70b8005ea955156e2cb1
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-02 13:48:15 +0530

    Create mathi28(02).C

---
+++ mathi28(02).C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+clrscr;
+int a[10],i,n;
+printf("enter the limimt");
+scanf("%d",&n);
+printf("enter the element");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the elements with index");
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",i,a[i]);
+return 0;getch;
+
+}
+}
