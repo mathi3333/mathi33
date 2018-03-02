3aba4a32af7b90db567399ad884b3f8bcf4bda51
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-02 14:26:15 +0530

    Create mathi34(02).C

---
+++ mathi34(02).C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+include<conoi.c>
+int main(void)
+{
+clrscr;
+	char str[50];
+	int i,count=1;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)\
+	{
+		if(str[i]=='.')
+	{
+			count=count+1;
+		}
+	}
+	printf("\nno of lines is=%d",count);
+	return 0;
+ getch;
+}
