86132e53b1e8feba0fec8757a101a549a72894d2
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-07 02:55:57 -0800

    Create MATHI50(07).C

---
+++ MATHI50(07).C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h
+int main(void)
+{
+clrscr;
+	int n;
+	scanf("%d",&n);
+  int i=2;
+	while(i<=n)
+	{
+		if(i==n)
+		{
+			printf("yes");
+		}
+		i=i*2;
+	}
+	return 0;
+  getch;
+}
