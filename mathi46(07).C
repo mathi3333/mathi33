849640f558913852c8384613552574e658517833
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-07 02:35:25 -0800

    Create mathi46(07).C

---
+++ mathi46(07).C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter tha first number");
+scanf("%d",&a);
+printf("the nex num of given value")
+b=a+1;
+scanf("d",&b);
+return 0;
+getch;
+}
