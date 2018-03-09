4bb23a02e4ae55210ccedaa548106a8eb9388c51
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-09 13:33:23 +0530

    Create mathi54(09).C

---
+++ mathi54(09).C
@@ -0,0 +1,16 @@
+#include<stdio.c>
+#include<conio.c>
+int main(void)
+{
+clrscr;
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("%d",n);
+	}
+	else
+	printf("%d",n-1);
+	return 0;
+  getch;
+}
