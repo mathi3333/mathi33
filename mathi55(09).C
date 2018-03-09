668e07d8d15ccde5f3079ad837c7ccfc5b80f185
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-09 13:37:58 +0530

    Create mathi55(09).C

---
+++ mathi55(09).C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+clrscr;
+	int n,m,t;
+	scanf("%d %d",&n,&m);
+	t=n*m;
+	if(t%2==0)
+	{
+		printf("even");
+	}
+	else
+	printf("odd");
+	return 0;
+  getch;
+}
