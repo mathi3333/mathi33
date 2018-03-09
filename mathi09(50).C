1a1aadcca4dc1041200cc16226dd29665d4617ab
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-09 13:25:42 +0530

    Create mathi09(50).C

---
+++ mathi09(50).C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+clrscr;
+	int n,sum=0,a;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		a=n%10;
+		sum=sum+a;
+		n=n/10;
+	}
+	printf("%d",sum);
+	return 0;
+  getch;
+}
