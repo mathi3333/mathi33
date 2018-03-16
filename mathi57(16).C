473a6c16f5e4fef32c885f3f0e3071e4243b8102
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-16 03:24:19 -0700

    Create mathi57(16).C

---
+++ mathi57(16).C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+clrscr;
+  int a,b,temp;
+  scanf("%d %d",&a,&b);
+  temp=a;
+  a=b;
+  b=temp;
+  printf("%d %d",a,b);
+  return 0;
+  getch;
+  }
