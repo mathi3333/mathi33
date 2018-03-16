49cebb77429a3e319e5c1ca845b5f63f3c6c1f07
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-03-16 04:02:52 -0700

    Create mathi60(16).C

---
+++ mathi60(16).C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+
+clrscr;
+
+
+    int f1=0,f2=1,f,n,i;
+    scanf("%d",&n);
+    printf("%d\t%d\t",f1,f2);
+    for(i=2;i<n;i++)
+{     f=f1+f2;
+       f1=f2;
+       f2=f;
+       printf("%d\t",f);
+}
+    return 0;
+    getch;
+}
+   
